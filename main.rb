0334c68dec1ebdbcaa9ae6ba7b99cfe7b14407e5
Lexis von Seggern <lexvon2015@gmail.com>
2017-08-23 13:31:12 -0500

    finally got the colors to save

--- main.rb
+++ main.rb
@@ -1,13 +1,29 @@
 require "sinatra"
+require "csv"
+require "pry"
+require "json"
+require "net/http"
+require "uri"
+
+
 
 get "/" do 
 	erb :index
 end 
 
-post "/store_painting" do
-	# create a function for saving the painting
-	# have an alert saying it was saved
-	# store that saved info in a csv file
+
+# GET /save_painting?savedArt=bwry,brrw,wwrb,wwwy
+get "/save_painting" do
+
+	colors = params['savedArt']
+	File.open("saves.txt", "a") do |line|
+		line.puts colors.chomp
+	end
+	erb :index
+	
 end
 
 # create post request to alert that it was saved
+# create a function for saving the painting
+	# have an alert saying it was saved
+	# store that saved info in a csv file
--- public/scripts.js
+++ public/scripts.js
@@ -12,19 +12,27 @@ window.addEventListener('load', function() {
 		});
 	}
 
-	// Save colors to csv
-	var savePainting = function(){
-		var boxColors =[];
-		for (var i = 0; i < rows.length; i++){
-			// if (rows[i].style.backgroundColor == ""){
-			// 	boxColors.push("white");
-			// } {
-			boxColors.push(rows[i].style.backgroundColor);
+	var save = document.getElementById('save_button');
+	save.addEventListener('click', function(){
+		var paintColors = [];
+		for (var i = 0; i < rows.length; i++) {
+			paintColors.push(rows[i].style.backgroundColor);
 		}
-		var data = "?saveArt=" + JSON.stringify(boxColors);
-		window.history.replaceState(null, null, "/" + data);
+		row1 = [paintColors[0], paintColors[1], paintColors[2], paintColors[3]]
+		row2 = [paintColors[4], paintColors[5], paintColors[6], paintColors[7]]
+		row3 = [paintColors[8], paintColors[9], paintColors[10], paintColors[11]]
+		row4 = [paintColors[12], paintColors[13], paintColors[14], paintColors[15]]
 
-		// saveToFile(boxColors)
-		return boxColors;
+		var request = new XMLHttpRequest();
+		request.open("GET", "/save_painting?savedArt=" + row1 + "," + row2 + "," + row3 + "," + row4);
+		request.send();
+		request.addEventListener("load", alert_Success);
+	});
+
+
+	var alert_Success = function(event){
+		alert("Success! Your painting was saved.");
+		var request = event.target;
 	}
+
 });
